3f8010c7409b1abe9db3e6507e76881914dcd531
Anatolii Polishchuk <anatolii.polishchuk@introduct.tech>
2025-01-08 14:01:58 +0200

    add tool-versions

--- lib/ex_banking/application.ex
+++ lib/ex_banking/application.ex
@@ -1,8 +1,4 @@
 defmodule ExBanking.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   @impl true
@@ -12,8 +8,6 @@ defmodule ExBanking.Application do
       # {ExBanking.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ExBanking.Supervisor]
     Supervisor.start_link(children, opts)
   end
